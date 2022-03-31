b2052479f42e9a1635b313b557a5c6722e35310b
Zoya <zoya@clearviewsys.com>
2022-03-31 11:20:15 -0700

    changed to headless

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -8,9 +8,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import adshopcart_locators as locators
+from selenium.webdriver.chrome.options import Options
 
-s = Service(executable_path='../chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+
+#s = Service(executable_path='../chromedriver.exe')
+#driver = webdriver.Chrome(service=s)
 
 
 # actions = ActionChains(driver=driver)
